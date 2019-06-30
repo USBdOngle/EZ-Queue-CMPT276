b9082e45b7c8a1b2a6208be7d34368bac3356ede
patricksnotstar <patricknguyen97@gmail.com>
2019-06-30 16:07:18 -0700

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome' => 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #gives rest of program access to models
   resources :guests, :moderators, :rooms
-  
+
 end
