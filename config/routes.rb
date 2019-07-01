00b898330fe7d5969a29a43ca617119cf77fac38
patricksnotstar <patricknguyen97@gmail.com>
2019-07-01 03:53:46 -0700

    routed other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome' => 'pages#home'
+  get 'join' => 'pages#join'
+  get 'create' => 'pages#create'
+  get 'loggedin' => 'pages#loggedin'
   #gives rest of program access to models
   resources :guests, :moderators, :rooms
 
