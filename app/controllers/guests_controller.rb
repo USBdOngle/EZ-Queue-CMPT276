1a363186798d363d3d7a4ac31b9ee52ead72ad55
aamritpa <51068276+aamritpa@users.noreply.github.com>
2019-07-02 21:21:09 -0700

    Update guests_controller.rb

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,2 +1,34 @@
 class GuestsController < ApplicationController
+
+    #just for showing the accessCode and password once the room is created
+    def show
+        @guest = Guest.find(params[:id])
+    end
+    
+    def new
+        @guest = Guest.new #this will be the argument required for the form which accepts the inputted password
+    end
+
+    def create
+        @guest = Guest.new(guest_params)
+        @guest.accessCode  = match_accessCode
+        if @guest.save
+            redirect_to @guest
+        else  
+            render 'new' #start process over again
+        end
+    end
+
+    private
+
+        def match_accessCode
+            while Guest.exists?(accessCode: Access Code) do
+                redirect_to @guest
+            end
+            render 'new' 
+        end
+
+        def guest_params
+            params.require(:guest).permit(:accessCode)
+        end
 end
