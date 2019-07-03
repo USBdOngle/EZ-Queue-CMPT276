8b99b36f6f7815eba54b1fc73e6f5f688830b8fc
naqsht <52226940+naqsht@users.noreply.github.com>
2019-07-02 21:31:19 -0700

    Update moderators_controller.rb

--- app/controllers/moderators_controller.rb
+++ app/controllers/moderators_controller.rb
@@ -1,2 +1,40 @@
-class ModeratorsController < ApplicationController
+class ModeratorController < ApplicationController
+
+    #just for showing the accessCode and password once the room is created
+    def show
+        @moderator = Moderator.find(params[:id])
+    end
+    
+    def new
+        @moderator = Moderator.new #this will be the argument required for the form which accepts the inputted password
+    end
+
+    def create
+        @moderator = Moderator.new(moderator_params)
+        @moderator.accessCode  = match_accessCode
+        @moderator.password  = match_password
+        if @moderator.save
+            redirect_to @moderator
+        else  
+            render 'new' #start process over again
+        end
+    end
+
+    private
+
+        def match_accessCode
+            while Moderator.exists?(accessCode: Access Code) do
+                redirect_to @moderator
+            end
+            while Moderator.exists?(password: Password) do
+                redirect_to @moderator
+            end
+            render 'new' 
+        end
+
+        def moderator_params
+            params.require(:moderator).permit(:name)
+            params.require(:moderator).permit(:accessCode)
+            params.require(:moderator).permit(:password)
+        end
 end
